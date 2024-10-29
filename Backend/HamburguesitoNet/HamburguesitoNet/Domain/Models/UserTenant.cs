5e8de1e4d70af55b2769b296d5bf417acb8b668a
Franco vazquez <fraan_v1@hotmail.com>
2024-10-29 13:51:19 +0100

    Add user tenant migration

---
+++ Backend/HamburguesitoNet/HamburguesitoNet/Domain/Models/UserTenant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Domain.Models
+{
+    public class UserTenant
+    {
+        [Key]
+        public int IdUser { get; set; }
+        public int IdTenant { get; set; }
+    }
+}
